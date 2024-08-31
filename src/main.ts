c0eb5ac5ed33f4ebddd51871081bfc9e8754ecc8
adriannebulao <amcbulao@gmail.com>
2024-09-01 01:45:14 +0800

    fix: made validation trigger properly by enabling ValidationPipe in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL,
   });
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('SAMAHAN')
