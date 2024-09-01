019a23c7546166d9af63fe9f8bb0f4e7c514cfe6
Rhaey <rhaeyislyfe@gmail.com>
2024-09-01 19:39:08 +0800

    feat: explicitly import config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -9,6 +10,7 @@ import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PrismaModule,
     SupabaseModule,
     AuthModule,
