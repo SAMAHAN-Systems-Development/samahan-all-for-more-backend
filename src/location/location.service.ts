7ac8fde027355e309b10aa35b7eb0a29f8688e31
Miggy Alino <miggyalino35@gmail.com>
2024-09-14 23:10:18 +0800

    feat(location): add delete endpoint

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateLocationDto } from './create-location.dto';
 import { UpdateLocationDto } from './update-location.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { isEmpty } from 'src/utils/utils';
 
 @Injectable()
 export class LocationService {
@@ -14,5 +15,39 @@ export class LocationService {
       },
     });
     return locations;
+  }
+
+  async remove(id: number) {
+    const location = await this.prismaService.location.findUnique({
+      where: { id },
+      include: { events: true },
+    });
+
+    if (!location) {
+      throw new HttpException(
+        `Location with id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    if (!isEmpty(location.events)) {
+      throw new HttpException(
+        `Location with id ${location.id} cannot be removed because it has existing events`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    try {
+      await this.prismaService.location.update({
+        where: { id },
+        data: { deleted_at: new Date() },
+      });
+      return { message: 'Location deleted successfully' };
+    } catch {
+      throw new HttpException(
+        'Error deleting location',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
