a6b8a76984b6788ff15b834549f4f174a0c7c975
Mark <mcac022803@gmail.com>
2024-09-20 10:29:12 +0800

    feat(event): add filter to ensure only future or current events are retrieved

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -7,9 +7,14 @@ export class EventService {
 
   async findAllEvents(page: number, limit: number) {
     const skip = (page - 1) * limit;
+    const currentDate = new Date();
+
     const totalEvents = await this.prisma.event.count({
       where: {
         deleted_at: null,
+        start_time: {
+          gte: currentDate,
+        },
       },
     });
 
@@ -18,6 +23,9 @@ export class EventService {
       take: limit,
       where: {
         deleted_at: null,
+        start_time: {
+          gte: currentDate,
+        },
       },
       include: {
         location: true,
